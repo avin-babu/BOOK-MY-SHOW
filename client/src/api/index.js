df0581381a7f535ecff3fa333599306a09d6bb07
avin-babu <avinbabuk12@gmail.com>
2025-09-12 08:23:06 +0530

    backend url has changed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const axiosInstance = axios.create({
-    baseURL: "http://host.docker.internal:3001",
+    baseURL: "http://backend-service:3001",
     headers: {
         "Content-Type": "application/json",
     }
